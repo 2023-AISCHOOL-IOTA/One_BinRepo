931b55e2b91655d1e2bc598425886d104fb3c96a
김성준 <korea95124@gmail.com>
2023-08-11 14:33:20 +0900

    0811SJcommit
    주석추가

--- WEB/project_html/project/final.py
+++ WEB/project_html/project/final.py
@@ -1,11 +1,13 @@
+# 필요한 모듈을 가져옵니다.
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import mysql.connector
 import logging
 import paho.mqtt.client as mqtt
 
+# Flask 애플리케이션을 생성합니다.
 app = Flask(__name__)
 
-# MySQL 데이터베이스 연결 설정
+# MySQL 데이터베이스 연결 설정을 정의합니다.
 db_config = {
     'host': 'project-db-stu3.smhrd.com',
     'user': 'Insa4_IOTA_hacksim_5',
@@ -14,9 +16,10 @@ db_config = {
     'port': 3307
 }
 
+# MySQL 데이터베이스에 연결합니다.
 db = mysql.connector.connect(**db_config)
 
-
+# 아이디 중복 확인 함수를 정의합니다.
 def is_duplicate_id(admin_id):
     cursor = db.cursor()
     sql = "SELECT COUNT(*) FROM admin WHERE ADMIN_ID = %s"
@@ -25,17 +28,17 @@ def is_duplicate_id(admin_id):
     cursor.close()
     return count > 0
 
-
+# 스플래시 페이지를 렌더링합니다.
 @app.route('/')
 def splash():
     return render_template('splash.html')
 
-
+# 로그인 페이지를 렌더링합니다.
 @app.route('/login_page')
 def index():
     return render_template('login1.html')
 
-
+# 로그인 처리를 합니다.
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
@@ -56,12 +59,12 @@ def login():
     else:
         return jsonify({"message": "아이디 or 비밀번호가 올바르지 않습니다."}), 401
 
-
+# 회원가입 페이지를 렌더링합니다.
 @app.route('/signup')
 def signup_page():
     return render_template('register.html')
 
-
+# 회원가입 처리를 합니다.
 @app.route('/signup', methods=['POST'])
 def signup():
     if request.method == 'POST':
@@ -83,12 +86,12 @@ def signup():
 
         return redirect(url_for('signup_success'))  # 회원가입 성공 페이지로 리다이렉트
 
-
+# 회원가입 성공 페이지를 렌더링합니다.
 @app.route('/signup_success')
 def signup_success():
-    return render_template('signup_success.html')  # 회원가입 성공 페이지 렌더링
+    return render_template('signup_success.html')
 
-
+# 아이디 중복 확인을 합니다.
 @app.route('/check_id_duplicate/<admin_id>')
 def check_id_duplicate(admin_id):
     cursor = db.cursor()
@@ -102,12 +105,7 @@ def check_id_duplicate(admin_id):
     else:
         return "not_duplicate"
 
-
-#@app.route('/connect')
-#def connect():
-#    return render_template('connect.html')
-
-
+# 쓰레기 용량 체크 함수
 def get_trash_capacity():
     try:
         conn = mysql.connector.connect(**db_config)
@@ -135,6 +133,7 @@ def get_trash_capacity():
 
 @app.route("/connect")
 def index_trash():
+    # 쓰레기통 연결 페이지
     capacity = get_trash_capacity()
     return render_template("connect.html", capacity=capacity)
 
@@ -169,6 +168,7 @@ def publish_mqtt_message(value):
 @app.route("/start", methods=["POST"])
 def start_trash():
     try:
+        # MQTT를 통해 쓰레기통 출발 메시지 발행
         publish_mqtt_message(1)
         return jsonify({"message": "쓰레기통 출발이 요청되었습니다."}), 200
     except Exception as e:
@@ -179,11 +179,13 @@ def start_trash():
 @app.route("/stop", methods=["POST"])
 def stop_trash():
     try:
+         # MQTT를 통해 쓰레기통 멈춤 메시지 발행
         publish_mqtt_message(0)
         return jsonify({"message": "쓰레기통 멈춤이 요청되었습니다."}), 200
     except Exception as e:
         return jsonify({"message": "쓰레기통 멈춤 요청을 실패했습니다."}), 500
 
 
+# 애플리케이션 실행
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
